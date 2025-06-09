56b9fdb529c82446f6e2ca5c9ba8b9e21be4b0ab
Jonathan Motta <jonathangmotta98@gmail.com>
2025-06-09 00:51:38 -0300

    Maint: update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, Extension
 import pybind11
 import sys
+from glob import glob
 
 extra_compile_args = ['-O3', '-march=native', '-fopenmp']
 extra_link_args = ['-fopenmp']
+
 
 if sys.platform == 'win32':
      extra_compile_args = ['/O2', '/openmp']
@@ -23,10 +25,18 @@ ext_modules = [
     ),
 ]
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='PyTerp',
     version='0.1.0',
     author='Jonathan Motta',
-    description='A package for performing optimized KNN IDW interpolation using C++.',
+    author_email='jonathangmotta98@gmail.com',
+    description='A package for performing optimized k-NN IDW interpolation using C++.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     ext_modules=ext_modules,
 )
