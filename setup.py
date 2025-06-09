8b651b28dcfd95109fb45b78511868e6cb0e8f96
Jonathan Motta <jonathangmotta98@gmail.com>
2025-06-09 16:07:02 -0300

    Maint: update pypi setup

--- setup.py
+++ setup.py
@@ -1,29 +1,21 @@
-from setuptools import setup, Extension
-import pybind11
+from setuptools import setup, Extension, find_packages
 import sys
-from glob import glob
+import pybind11
+import os
 
-extra_compile_args = ['-O3', '-march=native', '-fopenmp']
-extra_link_args = ['-fopenmp']
-
-
-if sys.platform == 'win32':
-     extra_compile_args = ['/O2', '/openmp']
-     extra_link_args = []
+_SRC_PATH = "src"
 
 
-ext_modules = [
-    Extension(
-        'PyTerp',
-        ['src/interpolator.cpp'],
-        include_dirs=[
-            pybind11.get_include(),
-        ],
-        language='c++',
-        extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args,
-    ),
-]
+cpp_extension = Extension(
+    name='pyterp',
+    sources=[os.path.join(_SRC_PATH, 'interpolator.cpp')],
+    include_dirs=[
+        _SRC_PATH,
+        pybind11.get_include(),
+    ],
+    language='c++',
+    extra_compile_args=['/O2', '/openmp'] if sys.platform == 'win32' else ['-O3', '-fopenmp'],
+)
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -31,12 +23,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 setup(
-    name='PyTerp',
+    name='pyterp',
     version='0.1.0',
     author='Jonathan Motta',
     author_email='jonathangmotta98@gmail.com',
     description='A package for performing optimized k-NN IDW interpolation using C++.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    ext_modules=ext_modules,
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    ext_modules=[cpp_extension],
+    zip_safe=False,
 )
